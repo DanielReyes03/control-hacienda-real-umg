a6238f60d4753234ae935340231896e0c6035131
DanielReyes03 <daniel_reyes2003@outlook.com>
2025-10-07 11:29:48 -0600

    Refactor: agregando nueva carpeta de compartidos

--- src/inventario/index.php
+++ src/inventario/index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <link rel="stylesheet" href="../compartido/componentes/cabecera/cabecera.css">
     <title>Inventario</title>
 </head>
 <body>
-    
+    <?php 
+        include("../compartido/componentes/cabecera/index.php");
+        cabecera("Inventario de Productos"); 
+    ?>
 </body>
 </html>
